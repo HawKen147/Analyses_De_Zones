1247c412e53f1c07ca38a281cb8e785f89b5af41
HawKen147 <112583131+HawKen147@users.noreply.github.com>
2023-12-01 15:38:01 +0100

    MAJ

--- window_class.py
+++ window_class.py
@@ -53,12 +53,12 @@ class App(customtkinter.CTk):
         self.creation_dossiers_label = customtkinter.CTkLabel(self, anchor='center', text="Arborescence des dossiers créer :\n CAM_XX \n         |_rampe \n        |_courir \n           |_marche \n                    |_incomplet.txt")
         self.nb_camera_entry = customtkinter.CTkEntry(self, width=250, placeholder_text="Nombre de dossier à créer")
         checked = customtkinter.IntVar()
-        self.verification_checkbox = customtkinter.CTkCheckBox(self, variable=checked, onvalue=1, offvalue=0, text="Cocher pour voir les fichiers manquants", command=self.get_checkbox)
-        self.chemin_dossier_label = customtkinter.CTkLabel(self, anchor='center', text="Entrer le chemin pour creer / stocker les videos")
-        self.chemin_video_label = customtkinter.CTkLabel(self, anchor='center', text="Entrer le chemin pour recuperer les videos de surveillance")
+        self.verification_checkbox = customtkinter.CTkCheckBox(self, variable=checked, onvalue=True, offvalue=False, text="Cocher pour voir les fichiers manquants", command=self.valider_button_event)
+        self.chemin_dossier_label = customtkinter.CTkLabel(self, anchor='center', text="Entrer le chemin pour créer / stocker les vidéos")
+        self.chemin_video_label = customtkinter.CTkLabel(self, anchor='center', text="Entrer le chemin pour récupérer les vidéos de surveillance")
         self.chemin_dossier_entry = customtkinter.CTkEntry(self, width=250, placeholder_text="Chemin pour stocker les vidéos")
-        self.chemin_video_entry = customtkinter.CTkEntry(self, width=250, placeholder_text="Chemin pour recuperer les vidéos")
-        self.valider_button = customtkinter.CTkButton(self, anchor='center', text="Valider")
+        self.chemin_video_entry = customtkinter.CTkEntry(self, width=250, placeholder_text="Chemin pour récupérer les vidéos")
+        self.valider_button = customtkinter.CTkButton(self, anchor='center', text="Valider", command=self.main_button_event)
     
         #position of the main_frame_widgets
         self.intro_label.grid(row=0, column=1, pady=(50,0), padx=5, sticky="nswe")
@@ -72,24 +72,43 @@ class App(customtkinter.CTk):
         self.valider_button.grid(row=8,column=1, pady=5)
 
         #definition initial state       
-        self.valider_button
+        self.valider_button.configure(state="disabled")
         self.appearance_mode_optionemenu.set("Dark")
         self.scaling_optionemenu.set("100%")
         self.bind_all("<KeyPress>", self.functions_calls)
 
     def check_path(self, path):
-        if ((os.path.exists(path) or path == '' )and self.nb_camera_entry.get() > '0'):
-            self.valider_button.configure(state="enable")
+        if ((os.path.exists(path) or path == '')):
             return True
         else :
-            self.valider_button.configure(state="disable")
             return False
+        
+    """def modify_entrys(self):
+         nb_camera = self.nb_camera_entry.get()
+         if nb_camera.isdigit():
+        """
 
-    def get_checkbox(self):
-        if self.verification_checkbox.get():
-            print('1')
+    def get_entrys(self):
+        nb_camera = self.nb_camera_entry.get()
+        chemin_stocker = self.chemin_dossier_entry.get()
+        chemin_get_videos = self.chemin_video_entry.get()
+        check_folders_checkbox = self.verification_checkbox.get()
+        print(check_folders_checkbox)
+        if check_folders_checkbox:
+            return True
+        elif nb_camera.isdigit() and os.path.exists(chemin_stocker) and len(os.listdir(chemin_stocker)) == 0 and chemin_get_videos == '' and not check_folders_checkbox :          # Verifier si nb_camera est bien un entier, si le chemin rensigner est existant et si le chemin renseigner est vide, si il n'y a rien d'entré dans lentré des videos surveillances et si la checkbox est bien décocher.
+            return True
+        elif nb_camera == '' and os.path.exists(chemin_stocker) and len(os.listdir(chemin_stocker)) > 0 and  os.path.exists(chemin_get_videos) and len(os.listdir(chemin_get_videos)) > 0 and not check_folders_checkbox :   #verifie si le nb de camera est null, si le chemin stocker existe et n'est pas vide, si le chemin pour les videos existe bien et n'est pas vide, si la checkbox n'est pas coché.
+            return True
+        
+            
+    def valider_button_event(self):
+        bool = self.get_entrys()
+        if bool :
+            self.valider_button.configure(state="normal")
         else :
-            print('0')
+            self.valider_button.configure(state="disabled")
+        #print(f"nb_cam = {nb_camera}, chemin stocker = {chemin_stocker}, chemin_get_videos {chemin_get_videos}")
 
 
     def open_input_dialog_event(self):
@@ -104,12 +123,26 @@ class App(customtkinter.CTk):
         customtkinter.set_widget_scaling(new_scaling_float)
 
     def sidebar_button_event(self):
-        print("sidebar_button click")
-    
+        print("sidebar_button click")   
+        
+    def main_button_event(self):
+        nb_camera = self.nb_camera_entry.get()
+        chemin_stocker = self.chemin_dossier_entry.get()
+        chemin_get_videos = self.chemin_video_entry.get()
+        if self.verification_checkbox.get():
+            #appel la fonction pour verifier tous les fichiers
+            print("check les fichiers manquants")
+        elif nb_camera != '' and chemin_stocker!= '':
+            #apppel la fonction pour créer les dossiers
+            fonctions.creer_dossier(nb_camera, chemin_stocker)
+            print("creer les dossiers")
+        elif chemin_stocker!= '' and chemin_get_videos != '':
+            #appel la fonction pour déplacer les videos des cameras
+            print("déplace les videos") 
     
     def functions_calls(self, event):
-        print(self.nb_camera_entry.get())
-        self.check_path(self.chemin_dossier_entry.get())
+       # self.check_path(self.chemin_dossier_entry.get())
+       self.valider_button_event()
     
     #def afficher_taille_frame(self, event=None):
     #    largeur_sidebar_frame = self.sidebar_frame.winfo_width()
