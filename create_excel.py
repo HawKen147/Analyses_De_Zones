2330f43748afdc45ec3f736c5718ddaefb9a920c
HawKen147 <112583131+HawKen147@users.noreply.github.com>
2024-11-04 11:25:39 +0100

    Update create_excel.py

--- create_excel.py
+++ create_excel.py
@@ -1,19 +1,15 @@
 from openpyxl import load_workbook
-import fonctions
-import openpyxl
+import fonctions as fonctions
 import datetime
 import locale
 import os
 
-
-#fonction test pour parcourir le fichier excel
-#ne sert a rien actuellement
 def parcour_num_cam_excel (sheet):
     # Parcourir la colonne B (index 2) à partir de la ligne 2 (en supposant que les en-têtes sont à la ligne 4)
     for cell in sheet.iter_rows(min_row=5, min_col=2, max_col=2):
         # Accéder à la valeur de la cellule actuelle
         valeur_cellule = cell[0].value  # Fonctionne si la cellule n'est pas vide
-        #print(valeur_cellule)
+        print(valeur_cellule)
 
 #definit la bonne colonne de la feuille excel selon le type de passage
 #return la lettre de la colonne de la feuille excel
@@ -52,56 +48,44 @@ def recuperer_heure_passage(nom_clip):
         
 #formate la date YYYY-MM-DD en jour xx mois année (vendredi 17 Mai 2024)
 #retourne la date formaté
-#####################################################################################################
-### Aujourd'hui cette fonction fait buguer la dépendance CustomTKinter pour des raisons inconnues ###
-### Pour changer la date il ne faut pas changer la variable locale (pour la date et l'heure)      ###
-#####################################################################################################
 def format_date(date):
-    """date_obj = datetime.datetime.strptime(date, "%Y-%m-%d") 
-    locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')                  #Transforme la date en francais (au lieu d'anglais)
-    formatted_date = date_obj.strftime("%A %d %B %Y")"""
-    return date
+    date_obj = datetime.datetime.strptime(date, "%Y-%m-%d") 
+    #locale.setlocale(locale.LC_ALL, 'fr_FR')                  #Transforme la date en francais (au lieu d'anglais) ############### Cette ligne fait bugger tout le script et ne dois pas etre utilisé
+    formatted_date = date_obj.strftime("%A %d %B %Y")
+    return formatted_date
 
 #fonction qui verifie si le dossier pour stoker le fichier excel existe ou pas
 #si il n'existe pas il crée le dossier
 def check_path():
-    if not "final" in os.listdir("excel"):
+    if not "final" in os.listdir(f"model/excel"):
         os.makedirs(f"excel/final")
 
 #Fonction principale de la création du fichier excel
-def main_excel ():
+def main_excel(path_to_folder):
 
     #fonction qui verifie si le dossier pour stoker le fichier excel existe ou pas
     #si il n'existe pas il crée le dossier
     check_path()
 
-    nb_excel_file = len(os.listdir(f"excel/final"))
-
-<<<<<<< Updated upstream
-
-def main ():
-    
-=======
->>>>>>> Stashed changes
     # Chemin du fichier Excel final
-    new_excel_file = "./excel/final/Essaies_Zones.xlsx"
+    new_excel_file = "./model/excel/final/Essaies_Zones.xlsx"
 
     # Charge le classeur Excel existant
-    workbook = load_workbook("./excel/model/model.xlsx")
+    workbook = load_workbook("./model/excel/model/model.xlsx")
 
     # Sélectionne la feuille active
     sheet = workbook.active
 
-    # Vérifier si le fichier final existe, si oui, renomme en conséquence
+    # Tant que le fichier excel existe, on change le nom du fichier en ajoutant l'indice au nombre de fichier
     i = 0
     while True:
         i += 1
         if not os.path.isfile(new_excel_file):
-            break # ajouter la fonction qui retourne l'erreur 
+            break 
         else:
-            new_excel_file = f"./excel/final/Essaies_Zones({i}).xlsx"
+            new_excel_file = f"./model/excel/final/Essaies_Zones({i}).xlsx"
 
-    path_to_folder = "C:/Users/pierry.benoit/Documents/dossiers_camera"
+    path_to_folder = "C:/Users/pierry.benoit/Documents/dossiers_camera"         #Il faut récuperer le chemin !
 
     # Obtenir le nombre de caméras
     nb_camera = fonctions.get_nb_camera(path_to_folder)
@@ -115,41 +99,16 @@ def main ():
 
     #On parcourt tout les clips
     for clip in clips :
-<<<<<<< Updated upstream
-        try :
-            for i in range (3):         #Chaque clip est dans une liste de 3
-                nom_clip = clip[i]
-                index_numero_cam = nom_clip.find("TH")                              #Index ou se situe TH (récuperer l'index de T)
-                numero_cam = nom_clip[index_numero_cam+2:index_numero_cam+4]        #On recupere l'indice ou se situe TH et on récupere les 2 chiffres apres TH
-                ligne_excel = int(numero_cam) + 4                                   #On commence a la 4è ligne du fichier excel (apres les en-tetes)
-                type_de_passage = nom_clip[-6:-4]                                   #On récupere le type de passage (qui est la fin du nom de la video ...DM.asf)
-                colonne_excel = colonne_passage_excel(type_de_passage)              #on récuperer la bonne colonne en fonction du passage (DM = colonne C)
-                date, heure = recuperer_heure_passage(nom_clip)
-                heure = heure.replace('h', ':')                                     #On remplace HHhMM en HH:MM (14h00 -> 14:00)
-                date = format_date(date)                                            #Formate la date en Francais
-                sheet[f"{colonne_excel}{ligne_excel}"] = f"{date} {heure}"          #Remplis la feuille de calcule
-
-        except Exception :                                                          #Gestion des éventuelles erreurs
-            #print(Exception)   #faire la fonction qui renvoie les erreurs
-            pass
-
-=======
         index_numero_cam = clip.find("TH")                              #Index ou se situe TH (récuperer l'index de T)
-        numero_cam = clip[index_numero_cam+2:index_numero_cam+4]  
-        print(f"numero des caméras : {numero_cam}")      #On recupere l'indice ou se situe TH et on récupere les 2 chiffres apres TH
+        numero_cam = clip[index_numero_cam+2:index_numero_cam+4]        #On recupere l'indice ou se situe TH et on récupere les 2 chiffres apres TH
         ligne_excel = int(numero_cam) + 4                                   #On commence a la 4è ligne du fichier excel (apres les en-tetes)
         type_de_passage = clip[-6:-4]                                   #On récupere le type de passage (qui est la fin du nom de la video ...DM.asf)
         colonne_excel = colonne_passage_excel(type_de_passage)              #on récuperer la bonne colonne en fonction du passage (DM = colonne C)
         date, heure = recuperer_heure_passage(clip)
         heure = heure.replace('h', ':')                                     #On remplace HHhMM en HH:MM (14h00 -> 14:00)
         date_time = format_date(date)                                       #Formate la date en Francais
-        sheet[f"{colonne_excel}{ligne_excel}"] = f"{date_time} {heure}"          #Remplis la feuille de calcule
->>>>>>> Stashed changes
+        new_date_time = fonctions.date_eng_to_fr(date_time)
+        sheet[f"{colonne_excel}{ligne_excel}"] = f"{new_date_time} {heure}"          #Remplis la feuille de calcule
 
     #On enregistre le classeur Excel
     workbook.save(new_excel_file)
-    new_nb_excel_file = len(os.listdir(f"excel/final"))
-    if (new_nb_excel_file > nb_excel_file):
-        return True
-    else :
-        False
